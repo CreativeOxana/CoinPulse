8ac33eb81d21790f46b7805b6b85e98ebc7adfa8
Oxana <creativesouloxana@gmail.com>
2026-01-02 16:53:06 +0100

    Adding Datatable, CoinView, TredingCoins

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import CoinOverview from "@/components/home/CoinOverview";
+import TrendingCoins from "@/components/home/TrendingCoins";
+import { Suspense } from "react";
 
-const Page = () => {
-  return <div></div>;
+const Page = async () => {
+  return (
+    <main className="main container">
+      <section className="home-grid">
+        <Suspense fallback={<>Loading Overview...</>}>
+          <CoinOverview />
+        </Suspense>
+
+        <Suspense fallback={<>Loading Trending...</>}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
 };
 
 export default Page;
